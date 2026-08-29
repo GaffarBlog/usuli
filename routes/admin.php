1ea228256ee75c5f0c16d2a9e5a5daf37c3d4c2c
topitbd <topitbd.git@gmail.com>
2026-08-29 18:11:04 +0600

    Update

--- routes/admin.php
+++ routes/admin.php
@@ -5,12 +5,14 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin', DashboardController::class)->name('admin.dashboard');
+Route::middleware('web')->group(function () {
+    Route::get('/admin', DashboardController::class)->name('admin.dashboard');
 
-Route::resource('admin/categories', CategoryController::class)
-    ->parameters(['admin/categories' => 'category'])
-    ->names('admin.categories');
+    Route::resource('admin/categories', CategoryController::class)
+        ->parameters(['admin/categories' => 'category'])
+        ->names('admin.categories');
 
-Route::resource('admin/posts', PostController::class)
-    ->parameters(['admin/posts' => 'post'])
-    ->names('admin.posts');
+    Route::resource('admin/posts', PostController::class)
+        ->parameters(['admin/posts' => 'post'])
+        ->names('admin.posts');
+});
